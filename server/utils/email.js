1ab3306d7a2c5e5aa22e05a8a92c41221aa87ed1
Madhusudan Mandale <madhusudanmandale387@gmail.com>
2026-09-21 22:47:31 +0530

    Fix: use Gmail SMTP port 587 with longer timeout

--- server/utils/email.js
+++ server/utils/email.js
@@ -4,11 +4,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 15000,
 });
 
 const sendBookingEmail = async (userEmail, userName, eventTitle) => {
